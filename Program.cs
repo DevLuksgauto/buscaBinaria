4c41668eac4bb0483a194422fee4f6dc6581644f
Lucas Gauto <lukscgauto@gmail.com>
2023-08-25 13:07:32 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int [] data = { 3, 17, 19, 26, 50, 52, 54, 69, 83, 84 };
+
+int searched_Number = 52;
+bool number_Found = false;
+int begin = 0;
+int end = data.Length - 1;
+int half;
+
+do
+{
+    half = begin + (end - begin) / 2;
+    if (data[half] == searched_Number)
+    {
+        Console.WriteLine("Number found at index " + half + ".");
+        number_Found = true;
+        break;
+    }
+    else if (data[half] > searched_Number)
+    {
+        end = half - 1;
+    }
+    else
+    {
+        begin = half + 1;
+    }
+} while (begin <= end);
+if (!number_Found)
+{
+    Console.WriteLine("Number not found.");
+}
